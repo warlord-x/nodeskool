4fd927923f26ef23cfa1c49fd701a522faffdad6
warlord-x <gaurang.developer@gmail.com>
2016-11-30 14:09:01 +0100

    HTTP CLIENT

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,30 +1,5 @@
-//console.log("HELLO WORLD");
-total = 0;
-for(i=2;i<process.argv.length;i++){
-  total = total + Number(process.argv[i]);
-}
-//console.log(total);
-var fs = require('fs');
-
-//fs.readdir(process.argv[2],callBackFileList);
-
-//var mymodule = require('./mymodule');
-//console.log(mymodule);
-
-//mymodule(process.argv[2],process.argv[3],displayList);
-
 var myclient = require('./myclient');
 myclient(process.argv[2],displayStreamData);
-
-function displayList(err,data){
-  //console.log(data);
-  data.filter(function (filename){
-
-console.log(filename);
-
-  });
-
-}
 
 function displayStreamData(err,data){
   console.log(data);
