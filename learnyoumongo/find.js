66b7d7eca22d3680e6ca7439a41b9b7592b60be8
warlord-x <gaurang.developer@gmail.com>
2016-12-06 11:46:53 +0100

    COUNT

--- learnyoumongo/find.js
+++ learnyoumongo/find.js
@@ -1,12 +1,10 @@
 var mongo = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/"+process.argv[2];
-mongo.connect(url,remove);
-function remove(err,db){
-  var docsCollection = db.collection(process.argv[3]);
-  //var person = {firstName:process.argv[2],lastName:process.argv[3]};
-  //console.log(JSON.stringify(person));
-  docsCollection.remove({
-  _id:process.argv[4]
+var url = "mongodb://localhost:27017/learnyoumongo";
+mongo.connect(url,count);
+function count(err,db){
+  var docsCollection = db.collection("parrots");
+  docsCollection.count({
+  age:{$gt:+process.argv[2]}
   },
   function(err, data) {
       console.log(data);
