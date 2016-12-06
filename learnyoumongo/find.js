a7b2f7493e59578c6288c4904756b32d4436ba27
warlord-x <gaurang.developer@gmail.com>
2016-12-06 11:04:26 +0100

    REMOVE

--- learnyoumongo/find.js
+++ learnyoumongo/find.js
@@ -1,19 +1,12 @@
 var mongo = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/"+process.argv[2];
-mongo.connect(url,update);
-function update(err,db){
-  var docsCollection = db.collection('users');
+mongo.connect(url,remove);
+function remove(err,db){
+  var docsCollection = db.collection(process.argv[3]);
   //var person = {firstName:process.argv[2],lastName:process.argv[3]};
   //console.log(JSON.stringify(person));
-  docsCollection.update({
-    name:"Tina",
-    age:30,
-    username:"tinatime"
-  },
-  {
-    $set:{
-      age:40
-    }
+  docsCollection.remove({
+  _id:process.argv[4]
   },
   function(err, data) {
       console.log(data);
