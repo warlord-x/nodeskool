9e549fcb7c2d659dc28f00561cd20a07b342b3df
warlord-x <gaurang.developer@gmail.com>
2016-12-06 11:00:25 +0100

    UPDATE

--- learnyoumongo/find.js
+++ learnyoumongo/find.js
@@ -1,14 +1,22 @@
 var mongo = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/learnyoumongo";
-mongo.connect(url,insert);
-function insert(err,db){
-  var docsCollection = db.collection('docs');
-  var person = {firstName:process.argv[2],lastName:process.argv[3]};
-  console.log(JSON.stringify(person));
-  docsCollection.insert(person,function(err, data) {
-      // handle error
-
-      // other operations
+var url = "mongodb://localhost:27017/"+process.argv[2];
+mongo.connect(url,update);
+function update(err,db){
+  var docsCollection = db.collection('users');
+  //var person = {firstName:process.argv[2],lastName:process.argv[3]};
+  //console.log(JSON.stringify(person));
+  docsCollection.update({
+    name:"Tina",
+    age:30,
+    username:"tinatime"
+  },
+  {
+    $set:{
+      age:40
+    }
+  },
+  function(err, data) {
+      console.log(data);
     });
 
   db.close();
