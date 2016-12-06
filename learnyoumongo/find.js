6fff7a6798799fbeadccd75bd37f943d99bfd63a
warlord-x <gaurang.developer@gmail.com>
2016-12-06 10:52:12 +0100

    INSERT

--- learnyoumongo/find.js
+++ learnyoumongo/find.js
@@ -1,17 +1,15 @@
 var mongo = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/learnyoumongo";
-mongo.connect(url,find);
-function find(err,db){
-  var parrots = db.collection('parrots');
-  parrots.find(
-    {
-      age:{$gt: parseInt(process.argv[2])}
-    },
-    {
-      name:1,age:1,_id:0
-    }
-  ).toArray(function(err, documents) {
-    console.log(documents);
-    })
+mongo.connect(url,insert);
+function insert(err,db){
+  var docsCollection = db.collection('docs');
+  var person = {firstName:process.argv[2],lastName:process.argv[3]};
+  console.log(JSON.stringify(person));
+  docsCollection.insert(person,function(err, data) {
+      // handle error
+
+      // other operations
+    });
+
   db.close();
 }
