471702b8430e2b2603efdcfb23a4660cb8f68dc9
warlord-x <gaurang.developer@gmail.com>
2016-12-06 12:27:20 +0100

    AGGREGATE

--- learnyoumongo/find.js
+++ learnyoumongo/find.js
@@ -1,14 +1,27 @@
 var mongo = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/learnyoumongo";
-mongo.connect(url,count);
-function count(err,db){
-  var docsCollection = db.collection("parrots");
-  docsCollection.count({
-  age:{$gt:+process.argv[2]}
-  },
-  function(err, data) {
-      console.log(data);
-    });
+mongo.connect(url,aggregate);
+function aggregate(err,db){
+  var docsCollection = db.collection("prices");
+
+  var match = {
+    $match  :  {size:process.argv[2]}
+  };
+
+  var group = {
+    $group : {
+      _id : 'average',
+      average : {
+        $avg : '$price'
+      }
+    }
+  };
+
+  docsCollection.aggregate([match,group]).toArray(printResults);
 
   db.close();
+}
+
+function printResults(err,data){
+  console.log(Number(data[0].average).toFixed(2));
 }
